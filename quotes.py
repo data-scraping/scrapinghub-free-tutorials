89d0d4c558cf0dd5e7ec458b0ca3907e319ca2db
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2020-08-01 21:58:23 +0800

    Scraping Multiple Items per Page

--- quotes.py
+++ quotes.py
@@ -4,12 +4,17 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/random']
+    start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
         self.log('I jus visited: ' + response.url)
-        yield{
-            'author_name' : response.css('small.author::text').extract_first(),
-            'text' : response.css('span.text::text').extract_first(),
-            'tags' : response.css('a.tag::text').extract()
-        }
+        # How to iterate over page elements
+        # How to extract data from repeating elements
+        for quote in response.css('div.quote'):
+            item = {
+                'author_name': quote.css('small.author::text').extract_first(),
+                'text': quote.css('span.text::text').extract_first(),
+                'tags': quote.css('a.tag::text').extract()
+            }
+            yield item
+
