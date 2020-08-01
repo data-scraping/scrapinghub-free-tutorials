55342bb72b71af538be15b4f7b86f942ba255973
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2020-08-01 22:15:11 +0800

    Following Pagination Links

---
+++ pagination.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class PaginationSpider(scrapy.Spider):
+    name = 'pagination'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        # follow pagination links
+        self.log('I jus visited: ' + response.url)
+        # How to iterate over page elements
+        # How to extract data from repeating elements
+        for quote in response.css('div.quote'):
+            item = {
+                'author_name': quote.css('small.author::text').extract_first(),
+                'text': quote.css('span.text::text').extract_first(),
+                'tags': quote.css('a.tag::text').extract()
+            }
+            yield item
+        next_page_url =  response.css('li.next > a::attr(href)').extract_first()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
